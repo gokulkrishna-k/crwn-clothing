cf219f09af36ac25c44078a732a1c736cd18f4ef
gokulkrishna-k <gokulkrishna.k.804@gmail.com>
2021-06-04 14:00:54 +0530

    added shop page ,collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 import { BrowserRouter, Route } from 'react-router-dom';
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+
 const App = () => {
   return (
     <BrowserRouter>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
     </BrowserRouter>
   );
 };
