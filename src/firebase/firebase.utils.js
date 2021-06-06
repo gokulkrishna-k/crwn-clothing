e76a776c26506a22886eff5f7fda8d93fdffd92d
gokulkrishna-k <gokulkrishna.k.804@gmail.com>
2021-06-06 15:59:15 +0530

    changed firebase config and added .env.local

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyAe5vVLRVgup3IxlxlIxeNuycnlv6APYds',
-  authDomain: 'crwn-clothing-gk.firebaseapp.com',
-  projectId: 'crwn-clothing-gk',
-  storageBucket: 'crwn-clothing-gk.appspot.com',
-  messagingSenderId: '1079905504106',
-  appId: '1:1079905504106:web:45df245b38d4f0b44b7313',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export const createUserProfileDocumnet = async (userAuth, aditionalData) => {
