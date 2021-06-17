301e1b32515208fdf03f52f6cf92fc6333e0311b
gokulkrishna-k <gokulkrishna.k.804@gmail.com>
2021-06-17 19:13:25 +0530

    updating with stipe and favicon

---
+++ src/components/stripe-button/stripe-button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51J3Kg6SCwLsJCkrED235e7YhfnJTyqgB7q5zGQQQ4kOi1Ok4jAsY8KHIHe1mdA6sM31DmhRgaBrt1N5MGHpNIDUb003TM7wKNG';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
